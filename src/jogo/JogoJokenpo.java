ac2225ec7ccb2cde22cfc75145f03e87814a7e28
Gab <gabriielgomess@outlook.com>
2021-09-02 16:25:12 -0300

    atualizando jogadas erroneas

--- src/jogo/JogoJokenpo.java
+++ src/jogo/JogoJokenpo.java
@@ -24,26 +24,32 @@ public class JogoJokenpo {
 		
 		//Decidir quem ganhou
 		
-		if (numeroSorteado == 1 && jogada == 1) {
+		if (jogada >=4) {
+			System.out.println("J O G A D A  I N V Á L I D A,  E S C O L H A  A P E N A S  A S  O P Ç Õ E S  E X I S T E N T E S!!");
+		} else if (jogada <=0) {
+			System.out.println("J O G A D A  I N V Á L I D A,  E S C O L H A  A P E N A S  A S  O P Ç Õ E S  E X I S T E N T E S!!");
+		}
+		
+		else if (numeroSorteado == 1 && jogada == 1) {
 			System.out.println();
 			System.out.println("Você escolheu: Pedra");
 			System.out.println("RandomBot escolheu: Pedra");
 			System.out.println();
-			System.out.println("Empate!!");
+			System.out.println("E M P A T E !!");
 			
 		}else if(numeroSorteado == 1 && jogada == 2) {
 			System.out.println();
 			System.out.println("Você escolheu: Papel");
 			System.out.println("RandomBot escolheu: Pedra");
 			System.out.println();
-			System.out.println("Você venceu RandomBot!!");
+			System.out.println("V O C Ê  V E N C E U  R A N D O M B O T!!");
 			
 		}else if (numeroSorteado == 1 && jogada == 3) {
 			System.out.println();
 			System.out.println("Você escolheu: Tesoura");
 			System.out.println("RandomBot escolheu: Pedra");
 			System.out.println();
-			System.out.println("RandomBot venceu!!");
+			System.out.println("R A N D O M B O T  V E N C E U!!");
 		}
 		
 		else if (numeroSorteado == 2 && jogada == 1) {
@@ -51,42 +57,42 @@ public class JogoJokenpo {
 			System.out.println("Você escolheu: Pedra");
 			System.out.println("RandomBot escolheu: Papel");
 			System.out.println();
-			System.out.print("RandomBot venceu!!");
+			System.out.print("R A N D O M B O T  V E N C E U!!");
 			
 		}else if (numeroSorteado == 2 && jogada == 2) {
 			System.out.println();
 			System.out.println("Você escolheu: Papel");
 			System.out.println("RandomBot escolheu: Papel");
 			System.out.println();
-			System.out.println("Empate!!");
+			System.out.println("E M P A T E!!");
 			
 		}else if (numeroSorteado == 2 && jogada == 3) {
 			System.out.println();
 			System.out.println("Você escolheu: Tesoura");
 			System.out.println("RandomBot escolheu: Papel");
 			System.out.println();
-			System.out.println("Você venceu RandomBot!!");
+			System.out.println("V O C Ê  V E N C E U  R A N D O M B O T!!");
 			
 		}else if (numeroSorteado == 3 && jogada == 1) {
 			System.out.println();
 			System.out.println("Você escolheu: Pedra");
 			System.out.println("RandomBot escolheu: Tesoura");
 			System.out.println();
-			System.out.println("Você venceu RandomBot!!");
+			System.out.println("V O C Ê  V E N C E U  R A N D O M B O T!!");
 			
 		}else if (numeroSorteado == 3 && jogada == 2) {
 			System.out.println();
 			System.out.println("Você escolheu: Papel");
 			System.out.println("RandomBot escolheu: Tesoura");
 			System.out.println();
-			System.out.println("RandomBot venceu!!");
+			System.out.println("R A N D O M B O T  V E N C E U!!");
 			
 		}else {
 			System.out.println();
 			System.out.println("Você escolheu: Tesoura");
 			System.out.println("RandomBot escolheu: Tesoura");
 			System.out.println();
-			System.out.println("Empate!!");
+			System.out.println("E M P A T E");
 		}
 		
 	}
