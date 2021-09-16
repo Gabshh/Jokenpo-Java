b38f04504751fbcc056e2e4193d1120ce00c0703
GABRIEL GOMES SAVIO <21276337@CFP127ALUNO.LOCAL>
2021-09-16 15:04:26 -0300

    Lógica finalizada

--- src/jogo/JogoJokenpo.java
+++ src/jogo/JogoJokenpo.java
@@ -9,8 +9,9 @@ public class JogoJokenpo {
 
 		Scanner leitor = new Scanner(System.in);
 
-		String resposta;
-		int jogada, numeroDePartidas;
+		String resposta = "S";
+		int jogada = 0;
+		int numeroDePartidas = 0;
 		int contador = 0;
 		int jogadasGanhasDoUser = 0;
 		int jogadasGanhasDoBot = 0;
@@ -19,13 +20,10 @@ public class JogoJokenpo {
 		System.out.println("--------------------------------------------------");
 		System.out.println("                 J O K E N P O");
 		System.out.println("--------------------------------------------------");
-		System.out.println();
-		System.out.print("Deseja desafiar RandomBot? (S/N): ");
-		resposta = leitor.next();
-		System.out.println();
 
 		while (resposta.equals("S") || resposta.equals("s")) {
 
+			System.out.println();
 			System.out.print("Quantas partidas deseja desafiar RandomBot? ");
 			numeroDePartidas = leitor.nextInt();
 
@@ -44,9 +42,16 @@ public class JogoJokenpo {
 				while (contador < numeroDePartidas && !(jogadasGanhasDoUser > (numeroDePartidas - empates) / 2
 						|| jogadasGanhasDoBot > (numeroDePartidas - empates) / 2)) {
 
-					System.out.println();
-					System.out.print("Qual será sua jogada? ");
-					jogada = leitor.nextInt();
+					while (!(jogada <= 3 && jogada >= 1)) {
+						System.out.println();
+						System.out.print("Qual será sua jogada? ");
+						jogada = leitor.nextInt();
+
+						if (!(jogada <= 3 && jogada >= 1)) {
+							System.out.println(
+									"J O G A D A  I N V Á L I D A,  E S C O L H A  A P E N A S  A S  O P Ç Õ E S  E X I S T E N T E S!!");
+						}
+					}
 
 					// o bloco de codigo abaixo define o resultado do jogo
 
@@ -131,10 +136,6 @@ public class JogoJokenpo {
 						System.out.println();
 						System.out.println("E M P A T E");
 						System.out.println("--------------------------------------------------");
-
-					} else if (jogada >= 4 || jogada <= 0) {
-						System.out.println(
-								"J O G A D A  I N V Á L I D A,  E S C O L H A  A P E N A S  A S  O P Ç Õ E S  E X I S T E N T E S!!");
 					}
 
 //				ESTABELECER QUE NÃO É MAIS POSSÍVEL O RIVAL SUPERA-LO
@@ -142,14 +143,16 @@ public class JogoJokenpo {
 					if (jogadasGanhasDoUser > (numeroDePartidas - empates) / 2) {
 						System.out.printf("NÃO É NECESSÁRIO PROSSEGUIR AS PARTIDAS!!", jogadasGanhasDoUser,
 								jogadasGanhasDoBot);
+						System.out.println();
 					} else if (jogadasGanhasDoBot > (numeroDePartidas - empates) / 2) {
 						System.out.printf("NÃO É NECESSÁRIO PROSSEGUIR AS PARTIDAS!!", jogadasGanhasDoBot,
 								jogadasGanhasDoUser);
+						System.out.println();
 					}
 					contador++;
-
+					jogada = 0;
 				}
-				if (jogadasGanhasDoUser == 0 && jogadasGanhasDoBot == 0) {
+				if (jogadasGanhasDoUser == jogadasGanhasDoBot) {
 					System.out.println();
 					System.out.println("CONSEGUIRAM EMPATAR " + contador + " VEZES :O" + "\nHORA DA PARTIDA DE OURO!!"
 							+ "\n Escolha cuidadosamente para o desempate...");
@@ -157,7 +160,7 @@ public class JogoJokenpo {
 
 //					PARTIDAS DE OURO
 
-					while (jogadasGanhasDoUser == 0 && jogadasGanhasDoBot == 0) {
+					while (jogadasGanhasDoUser == jogadasGanhasDoBot) {
 						System.out.println();
 						System.out.print("Qual será sua jogada? ");
 						jogada = leitor.nextInt();
@@ -252,35 +255,47 @@ public class JogoJokenpo {
 							System.out.println("--------------------------------------------------");
 						}
 
-						if (jogadasGanhasDoUser > jogadasGanhasDoBot) {
-							System.out.println();
-							System.out.printf("VOCÊ VENCEU O DESAFIO POR %d x %d", jogadasGanhasDoUser,
-									jogadasGanhasDoBot);
-						} else if (jogadasGanhasDoBot > jogadasGanhasDoUser) {
-							System.out.println();
-							System.out.printf("RANDOMBOT VENCEU O DESAFIO POR %d x %d", jogadasGanhasDoBot,
-									jogadasGanhasDoUser);
-						}
 					}
 				}
 			}
 			if (jogadasGanhasDoUser > jogadasGanhasDoBot) {
 				System.out.println();
 				System.out.printf("VOCÊ VENCEU O DESAFIO POR %d x %d", jogadasGanhasDoUser, jogadasGanhasDoBot);
+				System.out.println();
 			} else if (jogadasGanhasDoBot > jogadasGanhasDoUser) {
 				System.out.println();
 				System.out.printf("RANDOMBOT VENCEU O DESAFIO POR %d x %d", jogadasGanhasDoBot, jogadasGanhasDoUser);
+				System.out.println();
 			}
-			System.out.println();
-			System.out.print("Jogar novamente? (S/N): ");
-			resposta = leitor.next();
+			
+			resposta = "";
+			
+			while (!resposta.equalsIgnoreCase("s") && !resposta.equalsIgnoreCase("n")) {
+				System.out.print("Jogar novamente? (S/N) ");
+				resposta = leitor.next();
+				
+				if (!resposta.equalsIgnoreCase("s") && !resposta.equalsIgnoreCase("n")) {
+					System.out.println();
+					System.out.println("Por favor, responda corretamente!!");
+				}
+				
+				System.out.println();
+			}
+			
+			// Reinicializar placar
+
+			jogada = 0;
+			numeroDePartidas = 0;
+			contador = 0;
+			jogadasGanhasDoUser = 0;
+			jogadasGanhasDoBot = 0;
+			empates = 0;
 		}
+
 		if (resposta.equals("N") || resposta.equals("n")) {
 			System.out.println();
 			System.out.println("Ok, você pode jogar com RandomBot quando quiser! :)");
-		} else {
-			System.out.println();
-			System.out.println("Por favor, responda corretamente!!");
-		}leitor.close();
+		}
+		leitor.close();
 	}
 }
